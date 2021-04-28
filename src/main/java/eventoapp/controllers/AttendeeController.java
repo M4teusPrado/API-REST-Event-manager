2280fc04c5714d55e102847e9ceb8e92d716aa46
Mateus Prado <mateus.silvaprado@gmail.com>
2021-04-28 02:03:15 -0300

    Add controller Attendees

---
+++ src/main/java/eventoapp/controllers/AttendeeController.java
@@ -0,0 +1,54 @@
+package eventoapp.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import eventoapp.dto.AttendeeDTO;
+import eventoapp.models.Attendee;
+import eventoapp.services.AttendeeService;
+
+@RestController
+@RequestMapping("/attendees")
+public class AttendeeController {
+
+    @Autowired
+    private AttendeeService attendeeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AttendeeDTO> getAttendeeById(@PathVariable Long id ) {
+        return ResponseEntity.ok(attendeeService.getAttendeeById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Attendee> insertAttendee(@RequestBody Attendee Attendee)
+    {
+        Attendee aux = attendeeService.insertAttendee(Attendee);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(aux.getId()).toUri();
+        return ResponseEntity.created(uri).body(aux);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteAttendee(@PathVariable Long id) {
+        attendeeService.deleteAttendee(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @PutMapping("{id}") 
+    public ResponseEntity<AttendeeDTO> updateAttendee(@PathVariable Long id, @RequestBody AttendeeDTO AttendeeDTO)
+    {
+        AttendeeDTO dto = attendeeService.updateEvent(id, AttendeeDTO);
+		return ResponseEntity.ok().body(dto);
+    }
+
+}
