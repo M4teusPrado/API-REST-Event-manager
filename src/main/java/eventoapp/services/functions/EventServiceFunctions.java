002f05dade9e6b602d77d7ed4cfdb7920d0ff3c3
Mateus Prado <mateus.silvaprado@gmail.com>
2021-06-06 00:58:32 -0300

    Associação Place - Event - Com validação de Horario

--- src/main/java/eventoapp/services/functions/EventServiceFunctions.java
+++ src/main/java/eventoapp/services/functions/EventServiceFunctions.java
@@ -2,8 +2,8 @@ package eventoapp.services.functions;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -193,94 +193,39 @@ public class EventServiceFunctions implements EventService {
     public void verifyDateOfPlace(Event event, Place place) {
         List<Event> events = getEventsByPlace(place);
 
-        Boolean flag = true;
-
-        if (events.size() == 0)
-            return;
-
-        for (Event e : events) {
-            if  ((event.getStartDate().isAfter(e.getStartDate())    || (event.getStartDate().isEqual(e.getStartDate())))  &&
-                ((event.getStartDate().isBefore(e.getEndDate()))    || (event.getStartDate().isEqual(e.getEndDate())))){
-                    flag = false;
-                    break;
-            }
-
-            if  ((event.getEndDate().isAfter(e.getStartDate())    || (event.getEndDate().isEqual(e.getStartDate())))  &&
-                ((event.getEndDate().isBefore(e.getEndDate()))    || (event.getEndDate().isEqual(e.getEndDate())))){
-                    flag = false;
-                    break;
-            }
-
-            if (event.getStartDate().isBefore(e.getStartDate()) && event.getEndDate().isAfter(e.getEndDate())){
-                flag = false;
-                break;
-            }
-            // if(
-            //     e.getStartDate().isBefore(event.getStartDate())   && 
-            //     ( e.getEndDate().isAfter(event.getStartDate())    ||
-            //       e.getEndDate().isBefore(event.getStartDate()) 
-            //     )
-            // ){
-            //     flag = false;
-            //     break;
-            // }
-
-            // if(
-            //     ( e.getStartDate().isAfter(event.getStartDate())  ||
-            //       e.getStartDate().equals(event.getStartDate()))   &&
-            //     e.getEndDate().isBefore(event.getEndDate()) 
-            // ){
-            //     flag = false;
-            //     break;
-            // }
-
-            // if(
-            //     e.getStartDate().isEqual(event.getStartDate()) && 
-            //     e.getEndDate().isEqual(event.getEndDate()) 
-            // ){
-            //     flag = false;
-            //     break;
-            // }
-            
-            // if(
-            //     ( e.getStartDate().isBefore(event.getStartDate()) || 
-            //       e.getStartDate().equals(event.getStartDate())
-            //     )  
-            //       && 
-            //     ( e.getEndDate().isAfter(event.getEndDate()) || 
-            //       e.getEndDate().equals(event.getEndDate())
-            //     )
-            // ){
-            //     flag = false;
-            //     break;
-            // }
-
-            // if(
-            //     ( e.getStartDate().isAfter(event.getStartDate()) ||
-            //       e.getStartDate().isEqual(event.getStartDate()) 
-            //     ) 
-            //     &&
-            //     ( e.getEndDate().isBefore(event.getEndDate()) ||
-            //       e.getEndDate().isEqual(event.getEndDate())
-            //     )
-            // ){
-            //     flag = false;
-            //     break;
-            // }
-        }
-
-        if(!flag) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Horario invalido");
-        }
+        for (Event e : events)
+            if(!validDate(e, event)) 
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Horario invalido");
     }
 
     public List<Event> getEventsByPlace(Place place) {
         List<Event> events      = eventRepository.findAll();
-        List<Event> eventsAux   = new ArrayList<>();
-
-        for (Event e : events) 
-            if(e.getPlace(place.getId())) 
-                eventsAux.add(e);
+        List<Event> eventsAux = events.
+                                stream().
+                                filter(e -> e.getPlace(place.getId())).
+                                collect(Collectors.toList());
         return eventsAux;
+    }
+
+
+    private Boolean validDate(Event e, Event event) 
+    {
+        if ( event.getStartDate().isAfter(e.getStartDate()) && event.getStartDate().isBefore(e.getEndDate())) 
+            return false;
+
+        if ( event.getEndDate().isAfter(e.getStartDate()) && event.getEndDate().isBefore(e.getEndDate()))
+            return false;
+
+        if (event.getStartDate().isBefore(e.getStartDate()) && event.getEndDate().isAfter(e.getEndDate()))
+            return false;
+
+        if(event.getStartDate().equals(e.getEndDate()))
+            if(event.getStartTime().isBefore(e.getEndTime())) 
+                return false;
+            
+        if(event.getEndDate().equals(e.getStartDate()))
+            if(event.getEndTime().isAfter(e.getStartTime()))
+                return false;
+        return true;
     }
 }
